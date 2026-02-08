08664422a07202cc4632dc9599a1c755eba53364
Czar <ianaaronza@gmail.com>
2026-02-08 08:49:00 -0600

    Fix git name on vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/san-valetin-page/",
+  base: "/San-Valetin-Page/",
   plugins: [react()],
 })
